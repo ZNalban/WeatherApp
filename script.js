39c4f16c7918c9cff6fda55d034650df7ecc6e16
ZNalban <zpn11@outlook.com>
2024-12-25 16:11:31 +0400

    application style added

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     data = await response.json();
-    console.log(data.weather[0].main);
+    weather = data.weather[0].main;
+    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + `°C`;
     document.querySelector(".humidity").innerHTML = data.main.humidity + `%`;
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    // document.querySelector(".weather").innerHTML = weather;
 
     switch (data.weather[0].main) {
         case "Clear":
